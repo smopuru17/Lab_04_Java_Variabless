75a19ffb344117e02050aa4c5b764c362fce7634
smopuru17 <mopuruss@mail.uc.edu>
2023-09-16 13:08:34 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+       int intOperandA = 5;
+       int intOperandB = 1;
+
+       int intSum = 0;
+       int intProduct = 0;
+       int intDifference = 0;
+       int intQuotient = 0;
+       int intModulo = 0;
+       intSum = intOperandA + intOperandB;
+       System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The remainder of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+        double doubleOperandA = 25.1;
+        double doubleOperandB = 6.7;
+        double doubleSum = doubleOperandA + doubleOperandB;
+        double doubleProduct = doubleOperandA * doubleOperandB;
+        double doubleDifference = doubleOperandA - doubleOperandB;
+        double doubleQuotient = doubleOperandA / doubleOperandB;
+
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+
+
+
+        double lunchCost = 12.50;
+        int kids = 2;
+        double gasCost = 3.75;
+        int favoriteNumber = 17;
+        double shoeSize = 10.5;
+        String birthMonth = "June";
+        String fullName = "Srithan Sai Mopuru";
+
+
+
+
+        
+
+
+    }
+
+}
